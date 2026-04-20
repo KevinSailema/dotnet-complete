fed83e3fcc9f3bf4136c30b8238582b61830f695
Kevin Sailema <kevin.sailema@epn.edu,ec>
2026-04-20 17:52:17 -0500

    feat: first steps

---
+++ StructuredProgramming/04-Conditionals/Conditionals/Program.cs
@@ -0,0 +1,27 @@
+﻿var age = 12;
+
+if(age > 60)
+{
+    Console.WriteLine("Es de la tercera edad");
+}
+else if(age >18)
+{
+    Console.WriteLine("Es mayor de edad");
+}
+else
+{
+    Console.WriteLine("Es menor de edad");
+}
+
+switch (age)
+{
+    case < 18:
+        Console.WriteLine("Es menor de edad");
+        break;
+    case < 60:
+        Console.WriteLine("Es mayor de edad");
+        break;
+    default:
+        Console.WriteLine("Es de la tercera edad");
+        break;
+}
---
+++ StructuredProgramming/05-IterationStatements/Loops/Program.cs
@@ -0,0 +1,2 @@
+﻿// Conditional Statemets
+// Repetir sentencias
