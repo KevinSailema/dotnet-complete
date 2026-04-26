7b6d635e53f01d46571274d826489236017af2df
Kevin Sailema <kevin.sailema@epn.edu,ec>
2026-04-26 01:00:19 -0500

    feat: loops demo

--- StructuredProgramming/05-IterationStatements/Loops/Program.cs
+++ StructuredProgramming/05-IterationStatements/Loops/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Conditional Statemets
 // Repetir sentencias
+var names = new string[3]
+{
+    "Hector", "Juan", "Pedro"
+};
+
+
+int i = 0;
+do
+{
+    Console.WriteLine(names[i]);
+    i = i +1;
+} while (i <names.Length);
