256b0d23cf86d188c546b5312d57a6fb232ad48c
Kevin Sailema <kevin.sailema@epn.edu.ec>
2026-04-30 00:34:20 -0500

    fix: restructure

---
+++ HDL/StructuredProgramming/05-IterationStatements/Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿// Conditional Statemets
+// Repetir sentencias
+var names = new string[3]
+{
+    "Hector", "Juan", "Pedro"
+};
+
+
+int i = 0;
+// Por lo menos que se ejecute una vez Do-While
+do
+{
+    Console.WriteLine(names[i]);
+    i = i +1;
+} while (i <names.Length);
+
+// Mientras se cumpla la condición se ejecuta el bloque de código While
+while (i < names.Length)
+{
+    Console.WriteLine(names[i]);
+    i = i +1;
+}
+
+for (int j = 0; j < names.Length; j++)
+{
+    Console.WriteLine(names[j]);
+}
