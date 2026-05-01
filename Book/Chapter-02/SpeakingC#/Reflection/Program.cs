af9f1c0d1b235dba9554bf7a49383c0c9937ed8c
Kevin Sailema <kevin.sailema@epn.edu,ec>
2026-05-01 12:36:50 -0500

    fix: Chapter 02

--- Book/Chapter-02/SpeakingC#/Reflection/Program.cs
+++ Book/Chapter-02/SpeakingC#/Reflection/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Reflection;
+﻿using System.Reflection;
 
-class Program
+Assembly? myApp = Assembly.GetEntryAssembly();
+
+if (myApp is null) return;
+
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
 {
-    static void Main(string[] args)
+    Assembly a = Assembly.Load(name);
+    // Declare a variable to count the number of methods.
+    int methodCount = 0;
+    // Loop through all the types in the assembly.
+    foreach (TypeInfo t in a.DefinedTypes)
     {
-        Console.WriteLine("Hello, World!");
+    // Add up the counts of all the methods.
+    methodCount += t.GetMethods().Length;
     }
+    // Output the count of types and their methods.
+    Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount,
+    arg2: name.Name);
 }
--- Book/Chapter-02/SpeakingC#/Reflectionsss/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Reflectionsss;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
