30b921a40330a611ac95be1250c84d9f94db5461
Kevin Sailema <kevin.sailema@epn.edu,ec>
2026-05-15 17:35:16 -0500

    feat

---
+++ Fundamentals/02-Concepts/Structure/Program.cs
@@ -0,0 +1,11 @@
+﻿// Nueva estructura de proyecto en .NET 6, no es necesario declarar el namespace ni la clase Program, pero se puede hacer si se desea.
+// El punto y coma de namespace reemplaza a las llaves de apertura y cierre de la clase Program, y el método Main se declara directamente en el espacio de nombres.
+
+namespace Structure;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
